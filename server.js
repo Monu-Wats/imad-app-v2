57f2f49ccf8df92410a58fdfb945fa6547b1305a
Monu-Wats <monuwats1996@gmail.com>
2017-02-23 00:02:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,15 +18,15 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-    res.sendFile('article one has been delivered') ;  
+    res.send('article one has been delivered') ;  
 });
 
 app.get('/article-two',function(req,res){
-    res.sendFile('article two has been delivered');
+    res.send('article two has been delivered');
 });
 
 app.get('/article-three',function(req,res){
-    res.sendFile('article three has been delivered');
+    res.send('article three has been delivered');
 });
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
