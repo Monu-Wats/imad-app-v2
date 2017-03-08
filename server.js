b2d89887dfccf2505d57a108a3466df78703fd72
Monu-Wats <monuwats1996@gmail.com>
2017-03-08 12:31:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -62,6 +62,11 @@ function createTemplate(data)
     `;
     return htmlTemplate;
 }
+var counter = 0;
+app.get('/counter',function(req,res){
+   counter = counter + 1;
+   re.send(counter.toString());
+});
 app.get('/resume',function(req,res){
     res.sendFile(path.join(__dirname,'ui','resume.html'));
 });
