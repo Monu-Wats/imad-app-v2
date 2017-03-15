2789c4ba263ae8f2718d6f34f94471ba5fd7b48b
Monu-Wats <monuwats1996@gmail.com>
2017-03-15 16:12:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool = require('pg').Pool;
+var config = {
+    user : 'monu-wats',
+    database : 'monu-wats',
+    host : 'db.imad.hasura-app.io',
+    port : '5432',
+    password :  process.enu.DB_PASSWORD 
+    
+};
 var app = express();
 app.use(morgan('combined'));
 var articles = {
@@ -66,6 +74,19 @@ var counter = 0;
 app.get('/counter',function(req,res){
    counter = counter + 1;
    res.send(counter.toString());
+});
+var poop = new Pool(config);
+app.get('/test-db',function(req,res){
+   //make a select request 
+   //return response with the results
+   pool.query('SELECT * FROM test',function(err,result){
+      if(err){
+          res.status(500).send(err.toString());
+      }
+      else{
+          res.send(JSON.stringify(result.rows));
+      }
+   });
 });
 app.get('/resume',function(req,res){
     res.sendFile(path.join(__dirname,'ui','resume.html'));
